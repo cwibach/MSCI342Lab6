fb164f73a664e52573861b2d7c30f1eae544e1a0
cwibach <cwibach@uwaterloo.ca>
2023-03-13 10:47:07 -0400

    fixed lab8

--- client/src/components/App/index.js
+++ client/src/components/App/index.js
@@ -3,20 +3,21 @@ import Counter from '../../features/counter/Counter.js';
 import React, { Component } from 'react';
 
 
-const App = () => {
-  return(
-    <div>
-      <Counter />
-    </div>
-  );
- }
-// export default function App() {
-// return (
-//   <div>
-//     hello
-//     <RestaurantScreen />
-//   </div>
+// const App = () => {
+//   return(
+//     <div>
+//       <Counter />
+//     </div>
 //   );
-// }
+//  }
 
-export default App;
+export default function App() {
+return (
+  <div>
+    hello
+    <RestaurantScreen />
+  </div>
+  );
+}
+
+// export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -14,9 +14,7 @@ import { Provider } from 'react-redux';
 // );
 
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+    <App />,
   document.getElementById("root")
 );
 
